e02bb076a548ad22b9489d71c0ee3e59e3245c17
Björn <46351249+goanec82@users.noreply.github.com>
2023-09-29 15:35:31 +0200

    Update dkinstall.sh

--- dkinstall.sh
+++ dkinstall.sh
@@ -2,5 +2,6 @@ echo dl and install docker ...
 apt install docker.io curl -y
 curl -SL https://github.com/docker/compose/releases/download/v2.20.3/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
+docker volume create portainer_data
 sudo docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ee:latest
 echo https://192.168.178.27:9443/
